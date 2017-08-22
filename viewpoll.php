78a5735f9a148941c57588fbb5b9eaf515b0fd7e
Jake Demian <jakedemian@gmail.com>
2017-08-22 16:59:46 -0400

    set up skeleton for viewpoll.php

--- viewpoll.php
+++ viewpoll.php
@@ -1,11 +1,14 @@
-<html>
-    <head></head>
-    <body>
-        view poll
-        <?php
-            if(isset($_GET["id"])){
-                echo $_GET["id"];
-            }
-        ?>
-    </body>
-</html>
+<?php
+// other validation?
+if(!isset($_GET["id"])){
+    header("Location: /");
+}
+
+$phpFile = "";
+
+if(true){ // if user is locked out and can only view results OR if they clicked "View Results"
+
+} else { // otherwise, show the user the poll questions
+
+}
+
